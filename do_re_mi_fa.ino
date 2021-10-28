14a1f8a8336dc99c24eb257aecbf25846209085c
jvictorrp15 <93351925+jvictorrp15@users.noreply.github.com>
2021-10-28 19:01:32 -0300

    Add files via upload

---
+++ do_re_mi_fa.ino
@@ -0,0 +1,58 @@
+int buzzer = 8;//Atribui o valor 8 a variavel buzzer, que representa o pino digital 8, onde o buzzer está conectado
+
+void setup() 
+{
+pinMode(buzzer,OUTPUT);//Definindo o pino buzzer como de saída.
+}
+
+void loop()
+{
+    tone(buzzer,262,200);//Frequência e duração da nota Dó
+    delay(200);//Intervalo de 200 milissegundos
+    tone(buzzer,294,300);//Frequência e duração da nota Ré
+    delay(200);//Intervalo de 200 milissegundos
+    tone(buzzer,330,300);//Frequência e duração da nota Mi
+    delay(200);//Intervalo de 200 milissegundos
+    tone(buzzer,349,300);//Frequência e duração da nota Fá
+    delay(300);//Intervalo de 300 milissegundos
+    tone(buzzer,349,300);//Frequência e duração da nota Fá
+    delay(300);//Intervalo de 300 milissegundos
+    tone(buzzer,349,300);//Frequência e duração da nota Fá
+    delay(300);//Intervalo de 300 milissegundos
+    tone(buzzer,262,100);//Frequência e duração da nota Dó
+    delay(200);//Intervalo de 200 milissegundos
+    tone(buzzer,294,300);//Frequência e duração da nota Ré
+    delay(200);//Intervalo de 200 milissegundos
+    tone(buzzer,262,100);//Frequência e duração da nota Dó
+    delay(200);//Intervalo de 200 milissegundos
+    tone(buzzer,294,300);//Frequência e duração da nota Ré
+    delay(300);//Intervalo de 300 milissegundos
+    tone(buzzer,294,300);//Frequência e duração da nota Ré
+    delay(300);//Intervalo de 300 milissegundos
+    tone(buzzer,294,300);//Frequência e duração da nota Ré
+    delay(300);//Intervalo de 300 milissegundos
+    tone(buzzer,262,200);//Frequência e duração da nota Dó
+    delay(200);//Intervalo de 200 milissegundos
+    tone(buzzer,392,200);//Frequência e duração da nota Sol
+    delay(200);//Intervalo de 200 milissegundos
+    tone(buzzer,349,200);//Frequência e duração da nota Fá
+    delay(200);//Intervalo de 200 milissegundos
+    tone(buzzer,330,300);//Frequência e duração da nota Mi
+    delay(300);//Intervalo de 300 milissegundos
+    tone(buzzer,330,300);//Frequência e duração da nota Mi
+    delay(300);//Intervalo de 300 milissegundos
+    tone(buzzer,330,300);//Frequência e duração da nota Mi
+    delay(300);//Intervalo de 300 milissegundos
+    tone(buzzer,262,200);//Frequência e duração da nota Dó
+    delay(200);//Intervalo de 200 milissegundos
+    tone(buzzer,294,300);//Frequência e duração da nota Ré
+    delay(200);//Intervalo de 200 milissegundos
+    tone(buzzer,330,300);//Frequência e duração da nota Mi
+    delay(200);//Intervalo de 200 milissegundos
+    tone(buzzer,349,300);//Frequência e duração da nota Fá
+    delay(300);//Intervalo de 300 milissegundos
+    tone(buzzer,349,300);//Frequência e duração da nota Fá
+    delay(300);//Intervalo de 300 milissegundos
+    tone(buzzer,349,300);//Frequência e duração da nota Fá
+    delay(2500);//Intervalo de 2,5 segundos 
+}
